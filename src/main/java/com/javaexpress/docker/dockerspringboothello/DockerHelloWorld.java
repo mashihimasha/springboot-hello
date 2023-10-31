7ae2a74e7477cd6eb02a0473cbacfc758d53bb4c
Himashya Minoli <133405324+mashihimasha@users.noreply.github.com>
2023-11-01 04:06:34 +0530

    Update DockerHelloWorld.java

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -5,12 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/docker")
 public class DockerHelloWorld {
 
-	@GetMapping
-	public String getName() {
-		
+    	@GetMapping("/hello")
+	public String hello() {
 		return "Welcome to Deployment";
 	}
 }
